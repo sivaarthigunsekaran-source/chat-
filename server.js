41f671a98c91d7fb6e168d8533ccffcc78cc65c2
sivaarthigunasekaran <sivaarthigunsekaran@gmail.com>
2026-04-19 19:32:05 +0530

    Polyglot Chat app with 60+ language translation

--- server.js
+++ server.js
@@ -1,87 +1,54 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const mongoose = require("mongoose");
 const translate = require("google-translate-api-x");
-const path = require("path");
 
-// ===== MongoDB Model =====
-const MessageSchema = new mongoose.Schema({
-  sender: String,
-  text: String,
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-const Message = mongoose.model("Message", MessageSchema);
-
-// ===== App Setup =====
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-// ===== MongoDB Connection =====
-mongoose.connect("mongodb://127.0.0.1:27017/chat")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+const userLang = {};
 
-// ===== Store User Languages =====
-const userLanguages = {};
-
-// ===== Socket.IO =====
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
 
-  // Default language
-  userLanguages[socket.id] = "en";
+  userLang[socket.id] = "en";
 
   socket.on("set_language", (lang) => {
-    userLanguages[socket.id] = lang;
+    userLang[socket.id] = lang;
   });
 
-  socket.on("send_message", async ({ user, msg }) => {
+  socket.on("send_message", async (data) => {
 
-    // Save original message
-    const newMessage = new Message({
-      sender: user,
-      text: msg
-    });
-    await newMessage.save();
+    const clients = io.sockets.sockets;
 
-    // Send translated message to each user
-    for (const [id, sock] of io.sockets.sockets) {
+    for (let [id, client] of clients) {
 
-      const targetLang = userLanguages[id] || "en";
+      const lang = userLang[id] || "en";
 
       try {
-        const translated = await translate(msg, { to: targetLang });
+        const result = await translate(data.msg, { to: lang });
 
-        sock.emit("receive_message", {
-          user,
-          translatedMsg: translated.text
+        client.emit("receive_message", {
+          user: data.user,
+          msg: result.text
         });
 
       } catch (err) {
-        sock.emit("receive_message", {
-          user,
-          translatedMsg: msg
+        client.emit("receive_message", {
+          user: data.user,
+          msg: data.msg
         });
       }
     }
   });
 
   socket.on("disconnect", () => {
-    delete userLanguages[socket.id];
-    console.log("User disconnected:", socket.id);
+    delete userLang[socket.id];
   });
 });
 
-// ===== Start Server =====
 server.listen(5000, () => {
-  console.log("Server running at http://localhost:5000");
+  console.log("Server running on http://localhost:5000");
 });
